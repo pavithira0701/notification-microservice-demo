45e61377b584cefbfee25e5545f569e455991b31
Pavithira R R <pavithira.regupathy@gmail.com>
2026-09-21 14:16:48 +0530

    Fixed potential run time exceptions

--- src/test/java/com/example/demo/NotificationServiceApplicationTests.java
+++ src/test/java/com/example/demo/NotificationServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.notification.service.NotificationServiceApplication;
+
+@SpringBootTest(classes = NotificationServiceApplication.class)
 class NotificationServiceApplicationTests {
 
 	@Test
